6c6f07e8f317c53fab0b8bff429a4978e72fa847
unknown <=>
2019-11-22 16:31:40 +0100

    Avancée sur le design

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,9 +1,11 @@
+#Program libraries
 from grandpy.wikipedia import Wikipedia
 
 
 
 def test_get_right_pageid_on_wikipedia_from_coordinates():
     """Test of Wikipedia.get_pageid_from_coordinates()"""
+
     coord = {'lat': 43.296482, 'lng': 5.36978}
     wiki = Wikipedia(coord)
     wiki.get_pageid_from_coordinates()
